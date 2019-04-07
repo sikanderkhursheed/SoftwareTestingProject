ea9b58d222bcffe4231db41f691c77d70d612801
sikanderkhursheed <sikander.khursheed@gmail.com>
2019-04-06 17:58:03 -0700

    DuplicateCode check implemented to check duplication in code

---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/DuplicateCodeCheck.java
@@ -0,0 +1,57 @@
+package net.sf.eclipsecs.sample.checks;
+
+import java.util.ArrayList;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class DuplicateCodeCheck extends AbstractCheck{
+
+	@Override
+    public int[] getDefaultTokens() {
+        return new int[] { TokenTypes.CLASS_DEF};
+    }
+
+
+	@Override
+	public void visitToken(DetailAST detailast) {
+		if(detailast == null)
+		{
+			return;
+		}
+		
+			ArrayList<DetailAST> list = new ArrayList<DetailAST>();
+			DetailAST ast = detailast.findFirstToken(TokenTypes.OBJBLOCK).findFirstToken(TokenTypes.METHOD_DEF);
+			
+			while(ast != null) {
+				list.add(ast.findFirstToken(TokenTypes.SLIST));
+				ast = ast.getNextSibling();
+			}
+			
+			for(int i = 0; i < list.size(); i++) {
+				for(int j = i+1; j < list.size(); j++) {
+					if(list.get(i).equalsTree((list.get(j)))) {
+						log(ast, "duplicatecode", 0);
+						//detect();
+						
+					}
+				}
+			}
+		}	
+	@Override
+	public int[] getAcceptableTokens() {
+		return getDefaultTokens();
+	}
+	
+	@Override
+	public int[] getRequiredTokens() {
+		return getDefaultTokens();
+	}
+	
+	public int detect() {
+		return 1;
+	}
+	
+	
+}
